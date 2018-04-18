924141da0895e1c92fb3372cf82f08cf0d2d0294
Carlos Marquez <carl.marq95@gmail.com>
2018-04-17 20:41:20 -0600

    setup basic html document completed the purpose, audience, and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,9 +2,14 @@
 <html lang="en">
 <meta charset="utf-8">
 	<head>
-	<title>PWP</title>
+		<title>PWP</title>
 	</head>
 	<body>
-
+		<h1>Purpose, Audience, Goal</h1>
+		<p>Purpose: I would like for my website to be a gallery for graphic design
+		and for poetry.</p>
+		<p>Audience: The audience would be potential employers and professional peers.</p>
+		<p>Goal: My goal for this project is to serve as a gallery to demonstrate my skills
+		in web development, as well as to showcase my personal interests.</p>
 	</body>
 	</html>
