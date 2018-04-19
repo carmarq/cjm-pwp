6996ae5336fa632df7916d0a4e6e5ddd16f595a7
Carlos Marquez <carl.marq95@gmail.com>
2018-04-19 00:38:48 -0600

    setup basic html document completed the purpose, audience, and goal began persona added to persona and concluded

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,13 +10,24 @@
 		<p>Audience: The site is designed with a casual audience in mind. My target audience still includes the professional web development community, as well as recruiters.</p>
 		<p>Goal: My primary goal for this project is for it to serve as a gallery for my skills in web development. My secondary goal is to promote art.</p>
 		<h2>Persona</h2>
-	`	<br>
 		<p>Name:Gordon Gallegos</p>
-		<p>Age:42</p>
-		<p>Profession: Co-Founder of a few businesses: a horticulture business, a pub, and other side ventures. He needs someone to help manage his current websites</p>
-		<p>Technology Used: MacBook Pro 2013, and an iPhone 7</p>
-		<p>Behaviors/Attitudes:</p>
-		<p>Scenario:</p>
-		<p>Frustrations:</p>
+		<p>Age: 42</p>
+		<p>Profession: Co-Founder of a few businesses: a horticulture business, a sports bar, and other side ventures. He needs someone to help manage his current websites.</p>
+		<p>Technology Used: MacBook Pro 2013 (current OS), and an iPhone 6s Plus on a 4g unlimited network</p>
+		<p>Behaviors/Attitudes: Having to manage many businesses makes it difficult to waste time.
+			He typically wont spend longer than a few minutes looking over possible candidates for a position he needs to fill, but since this isn't a typical position he fills, he'll take a little extra time
+			to make sure a candidates a right fit. He's not on social media, really, and doesn't know much about IT, but knows what he's looking for.</p>
+		<p>Frustrations/Needs: He hopes to find candidates soon. He's frustrated with websites that have an overly-complex user-interface but appreciates creativity. He's also frustrated with his current system of handling his websites, and knows that he could do better.
+			He's really looking for someone for the long run but also needs someone to help promote what's going on right now, as soon as possible. More specifcally, he's looking for someone with previous experience, 2 to 3 years preferably. His
+			newer ventures are growing exponentially and needs someone to help promote his brands. He wants to hire within the next few weeks.</p>
+		<p>User Story: As a user I need to quickly determine if this candidate is suitable for managing websites, mostly, independently.</p>
+		<br>
+		<h2>Persona pt. 2</h2>
+		<p>Name: Keisha Santiago</p>
+		<p>Age: 20</p>
+		<p>Technology: Samsung Galaxy S7 on an unlimited 4g network, and a Dell Inspiron 15 3000 (Windows 10)</p>
+		<p>Scenario: She stumbled onto your website because the idea of poetry x graphic design sounds interesting to her.</p>
+		<p>Behaviors/Attitudes: Mostly, she appreciates functioning, quick, and clean websites. Lower quality, and slow websites turn her away from wanting to navigate them. Good content is also necessary to keep her coming back.</p>
+		<p>User Story: As a user, I want to navigate a website comfortably and enjoy the content.</p>
 	</body>
 	</html>
