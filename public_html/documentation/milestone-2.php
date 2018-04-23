131426fa1c5ec8409280096b215f3d502b647255
Carlos Marquez <carl.marq95@gmail.com>
2018-04-23 08:08:02 -0600

    added first part of content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8"
+	<head>
+		<title>Milestone 2a</title>
+	</head>
+	<body>
+		<p>
+			<strong>Content Strategy:</strong>
+		</p>
+		<p>Arcade/Intro: talks about the project, how it came about, who participated, and where we go from there.
+		I want this to be laid on an white background, looking similar to how a label looks in a museum</p>
+		<p>Galleria: I want the page to be long and continuous, showing graphic design and poetry as you scroll down. I would
+		also like to consider making the </p>
+		<p></p>
+	</body>
+</html>
