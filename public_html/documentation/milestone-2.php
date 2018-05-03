b72949ba24dd11afb5491aacace1c4a73e27213a
Carlos Marquez <carl.marq95@gmail.com>
2018-05-03 07:25:24 -0600

    added content strategy and wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,13 +5,10 @@
 		<title>Milestone 2a</title>
 	</head>
 	<body>
-		<p>
-			<strong>Content Strategy:</strong>
-		</p>
-		<p>Arcade/Intro: talks about the project, how it came about, who participated, and where we go from there.
-		I want this to be laid on an white background, looking similar to how a label looks in a museum</p>
-		<p>Galleria: I want the page to be long and continuous, showing graphic design and poetry as you scroll down. I would
-		also like to consider making the </p>
-		<p></p>
+		<p> <strong>Content Strategy:</strong>
+		<p>NOSTALGIA: A site dedicated for the user to be able to store memories and dreams digitally. Contains links to either log in or sign up. </p>
+		<p>Create a profile: sign-up only requires first and last name, username, and e-mail.</p>
+		<p>Remember page: Immediately after creating account you will be taken to a create memory page. You will be able to immediately write down something that you would like to remember. You can either write down a memory, access your memories, or customize your profile. These profiles are private and are for the user. </p>
+		<p>Memories page: Can be used with a few purposes. You can categorize your memories how you want, some categories will be given but an be changed. Your library of memories are timestamped, categorized, and can be locked even inside the app.</p>
 	</body>
 </html>
