0c3e47d5e4c164e715bcffe1954f962f5793828e
Carlos Marquez <carl.marq95@gmail.com>
2018-05-08 21:33:00 -0600

    edited punctuation

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -12,9 +12,9 @@
 		<h2>Persona</h2>
 		<p>Name:Gloria Gallegos</p>
 		<p>Age: 42</p>
-		<p>Profession: Gloria is an I.T. recruiter, and works for a smaller, technology business that is new to town.</p>
+		<p>Profession: Gloria is an I.T. recruiter for a smaller technology business that is new to town.</p>
 		<p>Technology Used: MacBook Pro 2013 (current OS), and an iPhone 6s Plus on a 4g unlimited network</p>
-		<p>Behaviors/Attitudes: Her experience in her career is extensive, and she's looking for the right fit for this new company. She cares about the company and wants to see it grow so she's looking for someone personable. She'll give a good look at a person's portfolio if they seem similar to the right fit. When searching personal sites, she looks for links to external sites like Github or LinkedIn.</p>
+		<p>Behaviors/Attitudes: Her experience in her career is extensive, and she's looking for the right fit for this new company. She cares about the company and wants to see it grow so she's looking for someone. She'll give a good look at a person's portfolio if they seem similar to the right fit. When searching personal sites, she looks for links to external sites like Github or LinkedIn.</p>
 		<p>Frustrations/Needs: She needs to find someone who is comfortable with working for a new business, with less benefits than a bigger, more established business. She's okay with newer talent, but is mostly looking for people who have been in the industry for at least 1 - 2 years. She can get frustrated looking at over stylized websites and mostly looks for simplicity.</p>
 		<p>User Story: As a user I need to quickly determine if this candidate is suitable for managing websites.</p>
 	</body>
