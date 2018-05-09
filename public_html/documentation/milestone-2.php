f42ded3e9c8ff48c518ed9cfc37e3a2e4a89a7ab
Carlos Marquez <carl.marq95@gmail.com>
2018-05-08 21:30:38 -0600

    added content strategy and wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <meta charset="UTF-8"
 	<head>
 		<title>Milestone 2a</title>
 	</head>
 	<body>
-		<p> <strong>Content Strategy:</strong>
-		<p>NOSTALGIA: A site dedicated for the user to be able to store memories and dreams digitally. Contains links to either log in or sign up. </p>
-		<p>Create a profile: sign-up only requires first and last name, username, and e-mail.</p>
-		<p>Remember page: Immediately after creating account you will be taken to a create memory page. You will be able to immediately write down something that you would like to remember. You can either write down a memory, access your memories, or customize your profile. These profiles are private and are for the user. </p>
-		<p>Memories page: Can be used with a few purposes. You can categorize your memories how you want, some categories will be given but an be changed. Your library of memories are timestamped, categorized, and can be locked even inside the app.</p>
+		<p> <strong> Content Strategy: </strong>
+		<p> <em> About-Me : Overall, I want my page to be simple. The about me page is going to have my name and a
+			text section underneath it. I also want to have my links to Twitter, Github, and LinkedIn easily accessible,
+			meaning that they'll be displayed center page along with the rest of the content. There will be a button
+			to move you directly to the Projects/Contact panel.</em </p>
+		<p><em> Contact : This section will be divided in two with the projects located on top and the contact form at the
+				bottom. I want to have an image of the capstone projects splash page, and include information about it. I'll
+				most likely leave it to display one project for simplicity and because my other projects are still being thought of
+				right now. The contact section will ask for your full name, email, subject, and allow you to send an email to me.
+				There will be a button to reset the forms to make them empty. I will also use captcha for verification. At the top of this page
+				there will be a button to pull you back to the top of the document or the about me section. </em></p>
+		<p><em> Mobile : The difference between desktop and mobile is that the desktop version will allow you to press the
+				buttons located on the top and bottom of the pages to redirect to those parts of the page. The Mobile
+				version scrolls sideways from left to right, with no endless scrolling. There will be circular identifiers
+				to let the user know what section they're on. </em></p>
+		<br>
+		<img src="About-me.png">
+		<br>
+		<img src="Contact.png">
+		<br>
+		<img src="Mobile.png">
 	</body>
 </html>
