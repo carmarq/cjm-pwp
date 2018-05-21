33d8be232331c1571e6b63de3b8a5579c5557746
Carlos Marquez <carl.marq95@gmail.com>
2018-05-20 18:11:24 -0600

    set up contact section

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
+		<!--Font Awesome Glyphs-->
+		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
 		<!--CSS Stylesheet-->
 <link rel="stylesheet" href="style.css" type="text/css">
 
@@ -35,8 +38,19 @@
 				</div>
 				<p>About Me:
 				<br>
-				Hi, I'm Carlos! I'm currently enrolled in a fullstack web development bootcamp. This page will be updated as time passes to document my work. Please look at my projects down below and feel free to contact me as well. Thank you!</p>
-				<div>
+				Hi, I'm Carlos! I'm currently enrolled in a fullstack web development boot camp. This page will be updated as time passes to document my work. Please look at my projects down below and feel free to contact me as well. Thank you!</p>
+						<div>
+					<a href="https://twitter.com/carjmarq" target="_blank">
+						<i class="fab fa-twitter-square fa-2x"></i>
+					</a>
+					<a href="https://www.linkedin.com/in/carjmarq/" target="_blank">
+						<i class="fab fa-linkedin fa-2x"></i>
+					</a>
+					<a href="https://github.com/carmarq/" target="_blank">
+						<i class=" fab fa-github-square fa-2x"></i>
+					</a>
+						</div>
+					<div>
 					<button>Projects</button>
 				</div>
 			</div>
@@ -49,18 +63,27 @@
 			<div class="row">
 				<div class="col-lg-6">
 				<h2>Projects</h2>
+				</div>
 				<p>Capstone:
 				<br>
 				For around two months I worked with friends on an application for the capstone of my coding boot camp. The application lets users look up Albuquerque breweries based on their search criteria for preference of drink. Those same breweries can moderate their own page, too.
 				</p>
 
-				</div>
 			</div>
 		</div>
 	</div>
 
 	<!--Contact Section Starts Here-->
+	<div class="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<h2>Contact</h2>
+				</div>
 
+			</div>
+		</div>
+	</div>
 
 
 
