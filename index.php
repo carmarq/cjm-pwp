bd69d80c6c87b86fbff2d375c0d8b9e2b60f11a3
Carlos Marquez <carl.marq95@gmail.com>
2018-05-30 13:35:48 -0600

    added form-validate file and links in header

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap	 		.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" 		crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" 		crossorigin="anonymous">
 
 		<!--Font Awesome Glyphs-->
 		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-
-		<!--FullPage.js CSS-->
-		<link rel="stylesheet" href="css/jquery.fullpage.min.css" type="text/css">
 
 		<!--CSS Stylesheet-->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
@@ -21,17 +18,21 @@
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 		integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 	 		crossorigin="anonymous"></script>
 
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" 		integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" 	 		crossorigin="anonymous"></script>
-
-		<!--FullPage.js-->
-		<script src="js/jquery.fullpage.min.js" type="text/javascript"></script>
-
-		<!--Custom JS needed to initialize fullPage-->
-		<script src="js/custom.js" type="text/javascript"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
 		<!--Google Fonts-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand">
 
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!--form validate js-->
+		<script src="js/form-validate.js"></script>
 
 		<title>Carlos Marquez, Junior Developer</title>
 
@@ -39,27 +40,27 @@
 
 <body>
 
-			<!--About Me Section Starts Here-->
+	<!--About Me Section Starts Here-->
 	<div class="dmscus">
 		<div class="container">
 			<div class="row">
-				<div class="col align-self-center">
-					<h1>Carlos
-						<br>
-						<span>Marquez</span>
-					</h1>
-				</div>
-					<p class="text-center">About Me:
-					<br>
-					Hi, I'm Carlos! I'm currently enrolled in a fullstack web development boot camp. This 					page will be updated as time passes to document my work. Please look at my projects 					down below and feel free to contact me as well. Thank you!</p>
-					<div class="col align-self-center">
-					<a href="https://twitter.com/carjmarq" target="_blank">
-						<i class="fab fa-twitter-square fa-lg"></i> </a>
-					<a href="https://www.linkedin.com/in/carjmarq/" target="_blank">
-						<i class="fab fa-linkedin fa-lg"></i> </a>
-					<a href="https://github.com/carmarq/" target="_blank">
-						<i class=" fab fa-github-square fa-lg"></i> </a>
-					</div>
+			<div class="col align-self-center">
+				<h1>Carlos
+		<br>
+		<span>Marquez</span>
+		</h1>
+		<p class="text-center">About Me:
+		<br>
+		Hi, I'm Carlos! I'm currently enrolled in a fullstack web development boot camp. This page will be updated as time passes to document my work. Please look at my projects down below and feel free to contact me as well. Thank you!</p>
+		<div class="col align-self-center">
+		<a href="https://twitter.com/carjmarq" target="_blank">
+			<i class="fab fa-twitter-square fa-lg"></i> </a>
+		<a href="https://www.linkedin.com/in/carjmarq/" target="_blank">
+			<i class="fab fa-linkedin fa-lg"></i> </a>
+		<a href="https://github.com/carmarq/" target="_blank">
+			<i class=" fab fa-github-square fa-lg"></i> </a>
+		</div>
+			</div>
 			</div>
 		</div>
 	</div>
@@ -68,17 +69,17 @@
 	<div class="projects">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12 col-sm-12 col-lg-12">
-					<h2 class="text-right">Projects</h2>
-				</div>
-					<p class="text-center">Capstone:
-					<br>
-					For about two months I worked with friends on an application for the capstone of my 					coding boot camp. The application lets users look up Albuquerque breweries based on 					specific search criteria for preference of drink. Those same breweries can moderate 					their own page, too.
-					</p>
-					<p class="text-center">Knights of Node:
-					<br>
-					From May 18th - May 20th, 2018 I participated in Angular Attack, a 48-hour hack-a-thon. 					A team of up to four has 48 hours to make any Angular application they want. The 					application that we made was a game where the player would take the role as the 					Angular knight and fight React dragons.
-				</p>
+			<div class="col-sm-12 col-sm-12 col-lg-12">
+				<h2 class="text-right">Projects</h2>
+		<p class="text-center">Capstone:
+		<br>
+		For about two months I worked with friends on an application for the capstone of my coding boot camp. The application lets users look up Albuquerque breweries based on specific search criteria for preference of drink. Those same breweries can moderate their own page, too.
+		</p>
+		<p class="text-center">Knights of Node:
+		<br>
+		From May 18th - May 20th, 2018 I participated in Angular Attack, a 48-hour hack-a-thon. A team of up to four has 48 hours to make any Angular application they want. The application that we made was a game where the player would take the role as the Angular knight and fight React dragons.
+		</p>
+			</div>
 			</div>
 		</div>
 	</div>
@@ -88,33 +89,26 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-12 col-md-12 col-lg-6 form">
-					<h2>Contact</h2>
+				<h2>Contact</h2>
+
+				<label for="full-name">Full Name</label>
+				<input type="text" id="Full-name" placeholder="Enter name here...">
+
+				<label for="e-mail">E-mail</label>
+				<input type="text" id="E-mail" placeholder="Enter E-mail here...">
+
+				<label for="subject">Subject</label>
+				<input type="text" id="subject" placeholder="Enter subject here...">
+
+				<label for="message">Message</label>
+				<textarea id="message" name="message" placeholder="Write something here..." style="12vh"></textarea>
 				</div>
-					<label for="full-name">Full Name</label>
-					<input type="text" id="Full-name" placeholder="Enter name here...">
-
-					<label for="e-mail">E-mail</label>
-					<input type="text" id="E-mail" placeholder="Enter E-mail here...">
-
-					<label for="subject">Subject</label>
-					<input type="text" id="subject" placeholder="Enter subject here...">
-
-					<textarea name="textarea" rows="8" cols="15">Write something here...</textarea>
 			</div>
 		</div>
 	</div>
 
 
-
-
-
-
-
-
-	</body>
-
-
-
+</body>
 
 
 
